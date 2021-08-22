fcda08aaae78423ea1c27ec5900b2249455e013c
suhail_khan28 <suhail.khan@walletdoc.com>
2021-08-22 22:58:12 +0530

    progress steps are created using Vanilla Javascript

---
+++ README.md
@@ -0,0 +1,3 @@
+# progress-steps-vanillajs
+
+Progress steps created using Vanilla JavaScript. You can check the live version on [CodePen](https://codepen.io/suhail369/pen/yLXBRLW)
---
+++ app.js
@@ -0,0 +1,48 @@
+//Selectors
+const progress = document.getElementById('progress');
+const circles = document.querySelectorAll('.circle');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+
+//Variable assign
+let currentActive = 1;
+
+//Event Listners
+next.addEventListener('click', () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
+})
+
+prev.addEventListener('click', () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+})
+
+//Functions
+const update = () => {
+    circles.forEach((circle, inx) => {
+        if (inx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%";
+
+    if (currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
